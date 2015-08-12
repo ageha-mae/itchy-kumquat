a32f263ec1f5daa62c3b2bc537269c82f0c57b7b
Apoorvaa Singh <apoorvaa.singh30@gmail.com>
2015-08-12 18:01:27 +0530

    Create BatchTest.java

---
+++ JDBC/BatchTest.java
@@ -0,0 +1,24 @@
+//Version 11.1
+
+
+import java.sql.*;
+class BatchTest
+{
+	public static void main(String s[])
+	{
+		try
+		{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","ducatWE1","abc123");
+		Statement st=con.createStatement();
+		st.addBatch("insert into student values('Lalit',102,99,'A')");
+		st.addBatch("insert into student values('Ajay',108,55,'C')");
+		st.addBatch("delete from student where rollno=101");
+		st.executeBatch();
+		System.out.println("Batch executed successfully");
+		}
+		catch(Exception e)
+		{	e.printStackTrace();
+		}
+	}
+}
