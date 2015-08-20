7675c1954755a92fb9f38f73ae2bcd25629aafe0
Apoorvaa Singh <apoorvaa.singh30@gmail.com>
2015-08-20 20:14:03 +0530

    Create LinkedHashSetTest.java

---
+++ Collections/LinkedHashSetTest.java
@@ -0,0 +1,28 @@
+//version 11.1
+
+import java.util.*;
+
+class LinkedHashSetTest
+{
+	public static void main(String s[])
+	{
+		LinkedHashSet al=new LinkedHashSet();
+	
+		al.add(new String("ABC"));
+		al.add("xyz");
+		al.add("mno");
+		al.add("pqr");
+		al.add("xyz");
+		al.add("hij");
+		
+		System.out.println("No. of elements:   "+al.size());
+		System.out.println("List of elements:   ");
+		Iterator itr=al.iterator();
+		while(itr.hasNext())
+		{
+			Object ob=itr.next();
+			System.out.println(ob);
+		}
+	}
+}
+
